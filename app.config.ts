ba88f05e10c4211152977be01a861267c801dc37
seraluce <seraluce@163.com>
2026-07-05 05:13:10 +0800

    子菜单

--- app.config.ts
+++ app.config.ts
@@ -2,5 +2,8 @@ export default defineAppConfig({
     ui: {
         font: false, //关闭字体
         css: ['~/assets/css/custom.css'], //自定义CSS
-    }
+    },
+    navigation: {
+        sub: 'header', // or 'aside'
+  },
   })
